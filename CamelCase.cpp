10608be128395562903d5d50c41e7960ba495d2b
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-09-08 00:28:09 +0900

    Update CamelCase.cpp

--- CamelCase.cpp
+++ CamelCase.cpp
@@ -1,1 +1,15 @@
+int camelcase(string s)
+{
+    int result = 1;
+    
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (std::isupper(s[i]) != 0)
+        {
+            result++;
+        }
+    }
+    
+    return result;
+}
 
