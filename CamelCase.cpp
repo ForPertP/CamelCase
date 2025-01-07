a981fa749516befbf40dc0a3439571431bb97f3d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-01-08 05:46:43 +0900

    Update CamelCase.cpp

--- CamelCase.cpp
+++ CamelCase.cpp
@@ -1,3 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
 int camelcase(string s)
 {
     int result = 1;
