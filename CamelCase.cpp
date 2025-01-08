1739de6f58cfb6f7568546bf06d5f19010edde22
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-01-09 06:17:27 +0900

    Update CamelCase.cpp

--- CamelCase.cpp
+++ CamelCase.cpp
@@ -24,3 +24,18 @@ int camelcase(string s)
     return result;
 }
 
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
