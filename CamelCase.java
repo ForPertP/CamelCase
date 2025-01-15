c7b508cf84b693c363d42e51c10fb87eaf962699
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-01-15 15:10:36 +0900

    Update CamelCase.java

--- CamelCase.java
+++ CamelCase.java
@@ -25,3 +25,20 @@ class Result {
     }
 
 }
+
+ublic class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
